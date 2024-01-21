6ecbbbbbdfd506def6bf3ad3215a805abf594c8e
Sophie Hopp <sophie.hopp@campus.lmu.de>
2024-01-21 18:12:36 +0100

    Add Subtract Operator

--- R/operators.R
+++ R/operators.R
@@ -12,3 +12,17 @@
 #' print(x)
 #' @export
 `%+=%` <- function(t, s) eval.parent(substitute(t <- t + s))
+
+#' @title Compound Assignment Subtract Operator
+#' 
+#' @description
+#' Subtract something from the left hand side in place.
+#' 
+#' @param t The left hand side that gets updated.
+#' @param s The right hand side that gets subtracted from the left hand side.
+#' @examples
+#' x <- 3
+#' x %-=% 2
+#' print(x)
+#' @export
+`%-=%` <- function(t, s) eval.parent(substitute(t <- t - s))
